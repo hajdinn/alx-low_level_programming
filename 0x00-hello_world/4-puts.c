ebe3a2b29349fa2b9b1333926c3238cfec32cf07
Halli John <hallianatejohn@gmail.com>
2023-08-10 14:17:52 -0700

    my first c programming commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Start for a program that print a line with puts function
+ *
+ * Return: 0 (successful program)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
