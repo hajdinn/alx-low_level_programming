7eb14e3c4604b2bf5a6012ecd52e8e1f52bdaf2b
Halli John <hallianatejohn@gmail.com>
2023-08-10 14:53:46 -0700

    task 5 on hello printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starting for a c program that use printf function
+ *
+ * Return: 0 (successfull program)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
