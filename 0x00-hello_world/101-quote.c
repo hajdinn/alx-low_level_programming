9e7e8c3d08503d4c024857aaeb50a72515ec3f7b
Halli John <hallianatejohn@gmail.com>
2023-08-10 16:14:06 -0700

    task 101 quote commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - start for a a C program that prints a line
+ *
+ * Retuen: 1 (successful program)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
