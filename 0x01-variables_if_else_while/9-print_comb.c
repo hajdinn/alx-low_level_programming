06d9a0e0ad4e36fd82382524116774c513aabf47
Halli John <hallianatejohn@gmail.com>
2023-08-11 16:31:14 -0700

    task 9 on printing combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - start of programn to print combination of numbers
+ *
+ * Return: 0 (program is a success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 10; x < 20; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x != 19)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
