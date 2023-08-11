434e17749ce8c26faae41484f5e647df83495d1f
Halli John <hallianatejohn@gmail.com>
2023-08-11 14:35:05 -0700

    task 3 to print both upper and lowercase commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting of program to print both lower an uppercase alphabet
+ *
+ * Return: 0 (program is a success)
+ */
+int main(void)
+{
+	char lowercase = a;
+	char uppercase = A;
+
+	for (lowercase a = 'a'; a <= 'z' ; lowercase a++)
+		putchar(lowercase a);
+	for (uppercase A = 'A'; a <= 'Z'; uppercase A++)
+		putchar(uppercase A);
+	putchar('\n');
+	return (0);
+}
