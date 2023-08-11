24d50f09dafbbc93cfaa0633e71eb5039e6135f8
Halli John <hallianatejohn@gmail.com>
2023-08-11 15:20:08 -0700

    task 4 on printing aphpabet and ending with new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - starting of program to print lowercase followed by newline
+ *
+ * Return: 0 (program is a success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' && a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
