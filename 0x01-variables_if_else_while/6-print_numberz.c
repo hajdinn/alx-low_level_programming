c2484d6da817a4cf603b011ad08a4aa712b1717a
Halli John <hallianatejohn@gmail.com>
2023-08-11 17:49:28 -0700

    task one on print number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - start of program to print number
+ * main - start of program to print numbers
  *
  * Return: 0 (program is a success)
  */
@@ -8,8 +8,8 @@ int main(void)
 {
 	int a = 0;
 
-	for (a < 10 ; a++)
-		purchar('0' + a);
+	for (; a < 10 ; a++)
+		putchar('0' + a);
 	putchar('\n');
 	return (0);
 }
