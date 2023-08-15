aacb538fa2b05b11d86362292ac1d066b8df1c5e
Halli John <hallianatejohn@gmail.com>
2023-08-15 04:34:16 -0700

    task 0 on putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - start of program 
+ * main - start of program to print putchar
  *
- * Return 0 (program is successful)
+ * Return: 0 (program success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,76 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/**
+ * _putchar - program that write char c to std
+ * @c: parameter to compare
+ * Return: 0
+ */
+int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - function to print alphabt 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+/**
+ * _islower - function that checks for lowercase character
+ * @c: character to be used
+ * Return: 0 or 1
+ */
+int _islower(int c);
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: character to be used
+ * Return 0 or 1
+ */
+int _isalpha(int c);
+/**
+ * print_sign - function that prints the sign of a number
+ * @n: integer to input
+ * Return: -1 or 0 0r 1
+ */
+int print_sign(int n);
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @int: integer
+ * Return: absolute value of int or integer
+ */
+int _abs(int);
+/**
+ * print_last_digit - function that prints the last digit of a number
+ * @int input integer
+ * Return: last digit
+ */
+int print_last_digit(int);
+/**
+ * jack_bauer - 24hours of jack bauer
+ */
+void jack_bauer(void);
+/**
+ * times_table - unction that prints the 9 times table
+ *
+ */
+void times_table(void);
+/**
+ * add - function that adds two integers
+ * @int: add integer
+ * Return: sum total
+ */
+int add(int, int);
+/**
+ * print_to_98 - print natural numbers to 98
+ * @n: integer to start
+ */
+void print_to_98(int n);
+/**
+ * print_times_table - function to print times table
+ * @n: integer to put
+ */
+void print_times_table(int n);
+#endif
