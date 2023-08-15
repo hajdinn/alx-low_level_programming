590c4d9a98e8100261a602db2f87912ddefa4a5d
Halli John <hallianatejohn@gmail.com>
2023-08-15 10:22:33 -0700

    task 11 to print all  natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n--)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
@@ -19,7 +19,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n--);
+				printf("%d, ", n--);
 			}
 		}
 	}
@@ -35,7 +35,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d, ", n);
 			}
 		}
 	}
