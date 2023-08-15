9360ddfc5724a02efbda2d8e57730b343e8b14b9
Halli John <hallianatejohn@gmail.com>
2023-08-15 16:10:13 -0700

    task 16 to finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,25 +5,43 @@
  */
 int main(void)
 {
-	int counter = 2;
+	int count;
+	unsigned long bacci1 = 0, bacci2 = 1, sum;
+	unsigned long hold11, hold12, hold21, hold22;
+	unsigned long hold1, hold2;
 
-	float x = 1;
-	float y = x + 1;
-	float z = x + y;
-
-	printf("%.0f, ", x);
-	printf("%.0f, ", y);
-	while (counter < 98)
+	for (count = 0; count < 92; count++)
 	{
-		counter++;
-		printf("%.0f", z);
-		x = y;
-		y = z;
-		z = x + y;
-		if (counter < 98)
+		sum = bacci1 + bacci2;
+		printf("%lu, ", sum);
+
+		bacci1 = bacci2;
+		bacci2 = sum;
+	}
+
+	hold11 = bacci1 / 10000000000;
+	hold21 = bacci2 / 10000000000;
+	hold12 = bacci1 % 10000000000;
+	hold22 = bacci2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		hold1 = bacci11 + bacci21;
+		hold2 = bacci12 + bacci22;
+		if (bacci12 + bacci22 > 9999999999)
 		{
-			printf(", ");
+			hold1 += 1;
+			hold2 %= 10000000000;
 		}
+
+		printf("%lu%lu", hold1, hold2);
+		if (count != 98)
+			printf(", ");
+
+		bacci11 = bacci21;
+		bacci12 = bacci22;
+		bacci21 = hold1;
+		bacci22 = hold2;
 	}
 	printf("\n");
 	return (0);
