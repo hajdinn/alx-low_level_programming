2835247411dcf274f0bc759b12163a8318128e2d
Halli John <hallianatejohn@gmail.com>
2023-08-15 16:19:31 -0700

    task 16 to finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,43 +5,30 @@
  */
 int main(void)
 {
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
 	int count;
-	unsigned long bacci1 = 0, bacci2 = 1, sum;
-	unsigned long hold11, hold12, hold21, hold22;
-	unsigned long hold1, hold2;
 
-	for (count = 0; count < 92; count++)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		sum = bacci1 + bacci2;
-		printf("%lu, ", sum);
-
-		bacci1 = bacci2;
-		bacci2 = sum;
-	}
-
-	hold11 = bacci1 / 10000000000;
-	hold21 = bacci2 / 10000000000;
-	hold12 = bacci1 % 10000000000;
-	hold22 = bacci2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		hold1 = bacci11 + bacci21;
-		hold2 = bacci12 + bacci22;
-		if (bacci12 + bacci22 > 9999999999)
+		if (bk1 + bk2 > 10000000000 || fr2 > 0 || fr1 > 0)
 		{
-			hold1 += 1;
-			hold2 %= 10000000000;
+			hold1 = (bk1 + bk2) / 10000000000;
+			hold2 = (bk1 + bk2) % 10000000000;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
 		}
-
-		printf("%lu%lu", hold1, hold2);
-		if (count != 98)
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
 			printf(", ");
-
-		bacci11 = bacci21;
-		bacci12 = bacci22;
-		bacci21 = hold1;
-		bacci22 = hold2;
 	}
 	printf("\n");
 	return (0);
