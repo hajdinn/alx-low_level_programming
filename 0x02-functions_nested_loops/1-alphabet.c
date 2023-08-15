b385a469520af70cfb4558665095e2197f1441f6
Halli John <hallianatejohn@gmail.com>
2023-08-15 04:58:55 -0700

    function to print alphabet a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - start of the alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
