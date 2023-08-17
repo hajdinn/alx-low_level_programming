572600b51efb7a8d97519b5f03821be86c193f03
Halli John <hallianatejohn@gmail.com>
2023-08-17 14:40:19 -0700

    task 3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function to print numbers
+ * Return 0 (success)
+ */ void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
