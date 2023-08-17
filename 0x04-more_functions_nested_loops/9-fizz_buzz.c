72d8e80f9f20add601b28e2ca0b84e3be8e47b89
Halli John <hallianatejohn@gmail.com>
2023-08-17 16:01:58 -0700

    task 9 to print fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - start of function to print fizz
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
