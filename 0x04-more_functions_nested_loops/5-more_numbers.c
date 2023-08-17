0edf718ca21092c22403b43c5598815ef4024324
Halli John <hallianatejohn@gmail.com>
2023-08-17 15:11:00 -0700

    task 5 on function that prints 10 times the numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - start of function to print more numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+			}
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
