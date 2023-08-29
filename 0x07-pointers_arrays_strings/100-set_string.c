23dceba9084fedf8feae442b45a678df65ea6bbd
Halli John <hallianatejohn@gmail.com>
2023-08-28 18:21:58 -0700

    task 8 for function that set value of pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - start of function that set value to pointer of char
+ * @s: string
+ * @to: value of string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
