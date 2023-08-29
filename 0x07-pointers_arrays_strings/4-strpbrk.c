f21f4fc4046164af6f2844db1bc6689f2758d00a
Halli John <hallianatejohn@gmail.com>
2023-08-28 17:46:14 -0700

    task 4 fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,7 +11,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; s[y] != '\0'; y++)
+		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (s[x] == accept[y])
 			{
