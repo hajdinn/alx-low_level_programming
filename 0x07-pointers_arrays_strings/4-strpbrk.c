239fadac00444e7361173642ad756530b8fb2a9b
Halli John <hallianatejohn@gmail.com>
2023-08-28 17:20:31 -0700

    task 4 to search for a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strpbrk - start of function that searches for string
+ * @accept: string bytes
+ * @s: string
+ * Return: value
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; s[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				return (s + x);
+			}
+		}
+	}
+	return (0);
+}
