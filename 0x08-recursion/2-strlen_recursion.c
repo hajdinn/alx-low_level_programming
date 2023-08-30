a6c40fa1b9d29dc694a440dc004500bfe8ae157e
Halli John <hallianatejohn@gmail.com>
2023-08-30 13:30:11 -0700

    Task 2 for function that print length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - start of function that that returns the length of string
+ * @s: lenth of string
+ * Return character
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i = _strlen_recursion(s + 1);
+		return (i += 1);
+	}
+	return (0);
+}
