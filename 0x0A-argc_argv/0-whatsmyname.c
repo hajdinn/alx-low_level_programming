c0771ddc97eee697e752ed751827d66a46d09f3b
Halli John <hallianatejohn@gmail.com>
2023-09-01 04:50:51 -0700

    task 0 for function that print a name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - start of function that print name of program
+ * @argv: array of argument
+ * @argc: argument number
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	unused(argc);
+	printf("%s\n", *argv);
+	return (0);
+}
