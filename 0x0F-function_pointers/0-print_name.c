6a840e6a0e41784e56d82c63bea709f31811ac3b
Halli John <hallianatejohn@gmail.com>
2023-09-13 16:09:20 -0700

    task 0 what is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - start of function to print name using pointer to function
+ * @name: name to print
+ * @f: pointer to function to use
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		return;
+
+	f(name);
+}
