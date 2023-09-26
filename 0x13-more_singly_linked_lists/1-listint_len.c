df0096b0155944a97ff1ce31fa905faf7ce50f1a
Halli John <hallianatejohn@gmail.com>
2023-09-26 11:48:44 -0700

    task 1 list lenght

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - length of linked lists
+ * @h: linked list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
